518eeeb78ed5519ce4cb8456635d26d0aef0fd21
Yash <189134048+yashraikwar1312@users.noreply.github.com>
2025-06-22 16:58:07 +0530

    Update ERCToken.sol

--- contracts/ERCToken.sol
+++ contracts/ERCToken.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ERCToken {
+    string public name = "ERCTOKEN";
+    string public symbol = "ERC";
+    uint8 public decimals = 18;
+    uint public totalSupply = 1000000 * 10 ** uint(decimals);
+    mapping(address => uint) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough tokens");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+}
