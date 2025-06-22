9c7d86c8b077a365b9b0e9722c5a77205c495359
Yash <189134048+yashraikwar1312@users.noreply.github.com>
2025-06-22 16:59:32 +0530

    Update Faucet.sol

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface IToken {
+    function transfer(address to, uint amount) external returns (bool);
+}
+
+contract Faucet {
+    address public tokenAddress;
+    address public owner;
+
+    constructor(address _tokenAddress) {
+        tokenAddress = _tokenAddress;
+        owner = msg.sender;
+    }
+
+    function sendTokens(address recipient) public {
+        IToken(tokenAddress).transfer(recipient, 100 * 10 ** 18);
+    }
+}
